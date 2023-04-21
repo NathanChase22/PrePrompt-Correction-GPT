ccf90ae436b9777415bba1d2f2419745a68a57d8
NathanChase22 <116322729+NathanChase22@users.noreply.github.com>
2023-04-20 22:47:43 -0500

    removed API key

--- EthicalGPT-master 2/src/main/java/net/chatgpt/testing/data/ChatGPTRequester.java
+++ EthicalGPT-master 2/src/main/java/net/chatgpt/testing/data/ChatGPTRequester.java
@@ -4,7 +4,7 @@ import net.chatgpt.testing.Main;
 
 public class ChatGPTRequester {
 
-    public static final String TOKEN = "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";
+    public static final String TOKEN = "<INSERT API KEY HERE>";
     public static boolean DEBUG_MODE = true;
     public static int requestCounter = 0;
 
